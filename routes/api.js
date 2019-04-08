2ad3def95c366a078dcb1e8655e55c5aabb29d73
John Williams <johnnjit7@gmail.com>
2019-04-07 22:39:52 -0400

    Can now split input based on units and number

--- routes/api.js
+++ routes/api.js
@@ -12,19 +12,26 @@ var expect = require('chai').expect;
 var ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
-  
+
   var convertHandler = new ConvertHandler();
 
   app.route('/api/convert')
     .get(function (req, res){
       var input = req.query.input;
-      var initNum = convertHandler.getNum(input);
-      var initUnit = convertHandler.getUnit(input);
+      var formatInput = input.replace(/\s+/g, '');
+      var indexOfFirstCharc = formatInput.search(/[A-Za-z]/);
+      console.log(indexOfFirstCharc);
+      var inputNumber = formatInput.slice(0, indexOfFirstCharc);
+      var inputUnit = formatInput.slice(indexOfFirstCharc);
+      console.log('num: ' + inputNumber + ' unit: ' + inputUnit);
+
+      var initNum = convertHandler.getNum(inputNumber);
+      var initUnit = convertHandler.getUnit(inputUnit);
       var returnNum = convertHandler.convert(initNum, initUnit);
       var returnUnit = convertHandler.getReturnUnit(initUnit);
       var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
-      
+
       //res.json
     });
-    
+
 };
