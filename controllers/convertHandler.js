dd8d8bcc2df3c1198872a14d2c069db129533250
John Williams <johnnjit7@gmail.com>
2019-04-09 18:41:40 -0400

    Working on convertHandler file

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -2,51 +2,57 @@
 *
 *
 *       Complete the handler logic below
-*       
-*       
+*
+*
 */
 
 function ConvertHandler() {
-  
+
   this.getNum = function(input) {
     var result;
-    
+    if(eval(input) !== undefined) {
+      result = eval(input);
+    }
+    else {
+      
+    }
+
     return result;
   };
-  
+
   this.getUnit = function(input) {
     var result;
-    
+
     return result;
   };
-  
+
   this.getReturnUnit = function(initUnit) {
     var result;
-    
+
     return result;
   };
 
   this.spellOutUnit = function(unit) {
     var result;
-    
+
     return result;
   };
-  
+
   this.convert = function(initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     var result;
-    
+
     return result;
   };
-  
+
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
     var result;
-    
+
     return result;
   };
-  
+
 }
 
 module.exports = ConvertHandler;
